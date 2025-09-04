0890c3750159a8418e894f52c244a5d019072792
SourovKarmokar <sourovkarmokar22@gmail.com>
2025-09-05 03:52:46 +0600

    post and get

--- 3forent/demo/src/App.jsx
+++ 3forent/demo/src/App.jsx
@@ -1,23 +1,54 @@
-import { useEffect, useState } from 'react'
-
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+import axios from "axios";
 
 function App() {
- useEffect(()=>{
-  fetch("http://localhost:3000/studentinfo")
-  .then((data) => console.log(data.json().then((res)=>console.log(res))
-  
-  )
-  )
- })
+  const [studentData, setStudentData] = useState([]);
+  const [ name , setName ] = useState("")
+  const [ email , setEmail ] = useState("")
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/studentinfo")
+      .then((response) => {
+        setStudentData(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, [studentData]);
+
+  console.log(studentData);
+
+  const handleCreate = () =>{
+    // console.log("ok cool");
+    // console.log(email);
+    // console.log(name);
+    axios.post("http://localhost:3000/createstudentinfo",{
+      name: name ,
+      email: email,
+    })
+  }
 
   return (
-    <>
-    <h1>
-      Student Datalist :
-    </h1>
-    </>
-  )
+    <div>
+      <h1>Student Datalist:</h1>
+
+      <div>
+        <input onChange={(e) => setName(e.target.value)} type="text" placeholder='Name' />
+        <br />
+        <input onChange={(e)=> setEmail(e.target.value)} type="email" placeholder='email' />
+        <br />
+        <button onClick={handleCreate}>Create</button>
+      </div>
+
+      {studentData.map((item, index) => (
+        <div key={index}>
+          <p>Name: {item.name}</p>
+          <p>Email: {item.email}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
