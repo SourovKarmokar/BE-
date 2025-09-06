e669a6d1f01dc3973d0a5cb8b017af0cc805a209
SourovKarmokar <sourovkarmokar22@gmail.com>
2025-09-07 02:52:02 +0600

    ok cool

--- 5/index.js
+++ 5/index.js
@@ -4,6 +4,8 @@ const userSchema = require("./model/userSchema");
 const app = express();
 app.use(express.json());
 const bcrypt = require("bcrypt");
+const emailVerification = require("./helpers/emailVerification");
+const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("ok cool");
@@ -12,24 +14,25 @@ app.get("/", (req, res) => {
 app.post("/createuser", (req, res) => {
   const { firstName, lastName, email, password } = req.body;
   bcrypt.hash(password, 10, function (err, hash) {
-    console.log(hash);
-    
+      const user = new userSchema({
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: hash,
+      });
+      user.save();
+      emailVerification()
+
+    console.log(hash, "hash");
   });
 
-  //   const user = new userSchema({
-  //     firstName: firstName,
-  //     lastName: lastName,
-  //     email: email,
-  //     password: password,
-  //   });
-//   user.save();
-//   res.status(201).json({
-//     message: "User Create Successfull",
-//     data: user,
-//   });
+    res.status(201).json({
+      message: "User Create Successfull",
+      data: user,
+    });
 });
 
-const port = 3000;
+
 connectDb();
 app.listen(port, () => {
   console.log("server is runnig");
