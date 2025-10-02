b0cdf556a6480895d18b74ee1d72e1818dafb0c4
SourovKarmokar <sourovkarmokar22@gmail.com>
2025-10-03 03:50:57 +0600

    otp

--- 7backend start/controller/registrationController.js
+++ 7backend start/controller/registrationController.js
@@ -1,46 +1,88 @@
 const emailValidation = require("../helpers/emailValidation");
 const userSchema = require("../model/userSchema");
 const bcrypt = require("bcrypt");
-function registrationController(req, res) {
-  console.log(req.body);
-  const { firstName, lastName, email, password } = req.body;
+const crypto = require("crypto");
 
-  if (!firstName) {
-    return res.json("First Name is Required");
-  }
-  if (!lastName) {
-    return res.json("Last Name is Required");
-  }
-  if (!email) {
-    return res.json("Email is Required");
-  }
-  if(!emailValidation(email)){
-    return res.json("thik moto email da")
-  }
-  if (!password) {
-    return res.json("Password is Required");
-  }
+async function registrationController(req, res) {
+  try {
+    const { firstName, lastName, email, password } = req.body;
 
-  bcrypt.hash(password, 10 , function (err, hash) {
+    // --- Basic Validations ---
+    if (!firstName) {
+      return res
+        .status(400)
+        .json({ success: false, message: "First Name is required" });
+    }
 
+    if (!lastName) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Last Name is required" });
+    }
+
+    if (!email) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Email is required" });
+    }
+
+    if (!emailValidation(email)) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid email format" });
+    }
+
+    if (!password) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Password is required" });
+    }
+
+    // --- Check for duplicate email ---
+    const existingUser = await userSchema.findOne({ email });
+    if (existingUser) {
+      return res
+        .status(409)
+        .json({ success: false, message: "Email already registered" });
+    }
+
+    // --- Generate OTP ---
+    const otp = crypto.randomInt(10000, 99999).toString();
+    console.log("Generated OTP:", otp);
+
+    const otpExpire = new Date(Date.now() + 5 * 60 * 1000);
+    console.log(otpExpire);
+
+    // --- Hash password ---
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // --- Create and save user ---
     const user = new userSchema({
-    firstName: firstName,
-    lastName: lastName,
-    email: email,
-    password: hash,
-     
-  });
-  user.save();
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: hashedPassword,
+      otp: otp,
+      otpExpire: otpExpire,
+    });
 
-  res.status(201).json({
-    message: "Registration Successfuly Done",
-    data: user,
-  });
-    
-  });
+    await user.save();
 
-  
-  
+    // --- Success response ---
+    return res.status(201).json({
+      success: true,
+      message: "Registration successful",
+      data: user,
+    });
+  } catch (err) {
+    console.error("Registration Error:", err);
+
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      error: err.message,
+    });
+  }
 }
 
 module.exports = registrationController;
