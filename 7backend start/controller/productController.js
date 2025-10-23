9e9cf5edf06830629c9c9f8cd623afd855ac12e2
SourovKarmokar <sourovkarmokar22@gmail.com>
2025-10-23 19:46:49 +0600

    Product

--- 7backend start/controller/productController.js
+++ 7backend start/controller/productController.js
@@ -1,11 +1,17 @@
 const productSchema = require("../model/productSchema");
 
 async function createProductController(req, res) {
+   
+    
+
+    
  try{
-       const {name, description, price, category , discount , stock } = req.body
+       const {name, description, price, category , discount , stock , image} = req.body
+
+       const imageName = req.file.filename
 
     const product = await new productSchema({
-        name, description, price, category, discount, stock
+        name, description, price, category, discount, stock , image : `http://localhost:3000/api/v1/${imageName}`
     })
     await product.save()
     res.status(201).json({
--- 7backend start/route/api/product.js
+++ 7backend start/route/api/product.js
@@ -1,7 +1,30 @@
 const express = require("express")
 const createProductController = require("../../controller/productController")
 const router = express.Router()
+const multer  = require('multer')
+// const upload = multer({ dest: 'uploads/' })
 
-router.post("/createproduct",createProductController)
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './upload')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.fieldname + '-' + uniqueSuffix + `.${file.originalname.split(".")[1]}`)
+    
+    console.log(null, file.fieldname + '-' + uniqueSuffix + `.${file.originalname.split(".")[1]}`);
+    console.log(file.originalname);
+    
+
+    console.log(file.originalname.split(".")[1]);
+    
+  }
+})
+
+const upload = multer({ storage: storage })
+
+
+router.post("/createproduct",upload.single('image'),createProductController)
 
 module.exports = router;
