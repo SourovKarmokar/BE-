dc390504797289fcd03eb0a9de0985bd9032b780
SourovKarmokar <sourovkarmokar22@gmail.com>
2025-10-31 02:32:08 +0600

    Category

--- ecommerce-dashboard/src/components/Category/AllCategories.jsx
+++ ecommerce-dashboard/src/components/Category/AllCategories.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 
-const AllCategories = () => {
+const invoices = [
+  {
+    invoice: "INV001",
+    paymentStatus: "Paid",
+    totalAmount: "$250.00",
+    paymentMethod: "Credit Card",
+  },
+]
+
+export default function AllCategories() {
   return (
-    <div>
-      This is all categories page
-    </div>
+    <Table>
+      
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">Invoice</TableHead>
+          <TableHead>Status</TableHead>
+          <TableHead>Method</TableHead>
+          <TableHead className="text-right">Amount</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {invoices.map((invoice) => (
+          <TableRow key={invoice.invoice}>
+            <TableCell className="font-medium">{invoice.invoice}</TableCell>
+            <TableCell>{invoice.paymentStatus}</TableCell>
+            <TableCell>{invoice.paymentMethod}</TableCell>
+            <TableCell className="text-right">{invoice.totalAmount}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+      
+    </Table>
   )
 }
 
-export default AllCategories
+
+
