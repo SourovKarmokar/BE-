da0b5acbbf2b35d9622b650b076bb5c73821b423
SourovKarmokar <sourovkarmokar22@gmail.com>
2025-10-31 03:57:54 +0600

    delete

--- ecommerce-dashboard/src/components/Category/AllCategories.jsx
+++ ecommerce-dashboard/src/components/Category/AllCategories.jsx
@@ -9,6 +9,11 @@ import {
   TableRow,
 } from "@/components/ui/table"
 
+import { Button } from "@/components/ui/button"
+import { useEffect } from "react"
+import axios from "axios"
+import { useState } from "react"
+
 const invoices = [
   {
     invoice: "INV001",
@@ -19,29 +24,60 @@ const invoices = [
 ]
 
 export default function AllCategories() {
+  const [categories, setCategories] = useState([])
+
+  const  fetchCategories =  async () =>{
+    try{
+      const {data} = await axios.get("http://localhost:3000/api/v1/category/getallcategories");
+      setCategories(data.data);
+      
+    }catch(error) {
+      console.log(error);
+      
+    }
+  }
+
+  useEffect(()=>{
+    fetchCategories()
+  },[])
+  
+  const handleDelete = (id) =>{
+    console.log(id);
+    axios.delete(`http://localhost:3000/api/v1/category/deletecategory/${id}`);
+    fetchCategories();
+  }
+  
   return (
-    <Table>
+    <div className="mx-10 shadow rounded py-2">
+      <Table>
       
       <TableHeader>
         <TableRow>
-          <TableHead className="w-[100px]">Invoice</TableHead>
-          <TableHead>Status</TableHead>
-          <TableHead>Method</TableHead>
-          <TableHead className="text-right">Amount</TableHead>
+          <TableHead className="w-[100px]">Sr.</TableHead>
+          <TableHead>Name</TableHead>
+          <TableHead>Description</TableHead>
+          <TableHead >Action</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
-        {invoices.map((invoice) => (
-          <TableRow key={invoice.invoice}>
-            <TableCell className="font-medium">{invoice.invoice}</TableCell>
-            <TableCell>{invoice.paymentStatus}</TableCell>
-            <TableCell>{invoice.paymentMethod}</TableCell>
-            <TableCell className="text-right">{invoice.totalAmount}</TableCell>
+        {categories.map((category,index) => (
+          <TableRow key={category._id}>
+            <TableCell className="font-medium">{index+1}</TableCell>
+            <TableCell>{category.name}</TableCell>
+            <TableCell>{category.description}</TableCell>
+            <TableCell>
+              <Button className="mr-3 ">Edit</Button>
+            <Button 
+            onClick={()=>handleDelete(category._id)}
+            className="bg-red-500" >Delete</Button>
+            </TableCell>
+           
           </TableRow>
         ))}
       </TableBody>
       
     </Table>
+    </div>
   )
 }
 
